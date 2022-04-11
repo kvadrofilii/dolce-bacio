548bda930a5e061ac37432a2c8927ca1db89360f
Michael Yakovlev <kvadrofilii@gmail.com>
2022-04-11 11:04:25 +0300

    refactor: script.js

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 'use strict';
 
-const body = document.querySelector('body');
 const pageHeader = document.querySelector('.page-header');
 const hamburgerList = pageHeader.querySelector('.hamburger__list');
 const hamburgerBtn = pageHeader.querySelector('.hamburger__btn');
 
-hamburgerBtn.addEventListener('click', () => {
-	toggleClass();
-})
+hamburgerBtn.addEventListener('click', () => toggleClass());
 
 hamburgerList.addEventListener('click', (event) => {
-	const target = event.target;
+  const target = event.target;
 
-	if (target.tagName !== 'A') return;
-	toggleClass();
-})
+  if (target.tagName !== 'A') return;
+  toggleClass();
+});
 
 function toggleClass() {
-	hamburgerList.classList.toggle("hamburger__toggle");
-	pageHeader.classList.toggle("hamburger__toggle");
-	body.classList.toggle("hamburger__toggle");
+  hamburgerList.classList.toggle("hamburger__toggle");
+  pageHeader.classList.toggle("hamburger__toggle");
+  document.body.classList.toggle("hamburger__toggle");
 }
