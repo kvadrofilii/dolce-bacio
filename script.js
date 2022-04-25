07bf0da219f49808a760c65269e1f7b6b810b678
Michael Yakovlev <kvadrofilii@gmail.com>
2022-04-25 12:51:04 +0300

    refactor: burger menu toggle & addEventListener

--- script.js
+++ script.js
@@ -6,15 +6,19 @@ const pageHeader = document.querySelector('.page-header'),
 
 hamburgerBtn.addEventListener('click', () => toggleClass());
 
-hamburgerList.addEventListener('click', (e) => {
-  const target = e.target;
-
-  if (target.tagName !== 'A') return;
+hamburgerList.addEventListener('click', e => {
+  // Получаю нажатую ссылку
+  const link = e.target.closest('.hamburger__link');
+  // Если нажали мимо ссылки, то прерываю функцию
+  if (!link) return;
+  // Закрываю меню
   toggleClass();
 });
 
 function toggleClass() {
-  hamburgerList.classList.toggle("hamburger__toggle");
-  pageHeader.classList.toggle("hamburger__toggle");
-  document.body.classList.toggle("hamburger__toggle");
+  hamburgerList.classList.toggle('hamburger__toggle');
+  pageHeader.classList.toggle('hamburger__toggle');
+  document.body.style.overflow === 'hidden' ?
+    document.body.style.overflow = '' :
+    document.body.style.overflow = 'hidden';
 }
